ec9869ab9487d15ac2458c79a0ba50ebda2f41dc
sherrygamerz <111529472+sherrygamerz@users.noreply.github.com>
2025-05-18 00:56:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -36,4 +36,5 @@ def index():
     return render_template('index.html', thumbnail_url=thumbnail_url)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
