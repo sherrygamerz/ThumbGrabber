4027f1d89aa3d91849a9f71ad89158d07235aef1
sherrygamerz <111529472+sherrygamerz@users.noreply.github.com>
2025-05-21 11:13:12 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,11 @@
 from flask import Flask, render_template, request
 from urllib.parse import urlparse, parse_qs
-import os  # Fix: added import for os
+import os
 
 app = Flask(__name__)
 
 def extract_video_id(url):
-    """
-    Supports:
-    - https://www.youtube.com/watch?v=VIDEO_ID
-    - https://youtu.be/VIDEO_ID
-    - https://youtube.com/shorts/VIDEO_ID
-    """
     parsed_url = urlparse(url)
-
     if 'youtube' in parsed_url.netloc:
         if parsed_url.path == '/watch':
             return parse_qs(parsed_url.query).get('v', [None])[0]
@@ -20,21 +13,25 @@ def extract_video_id(url):
             return parsed_url.path.split('/')[2]
     elif 'youtu.be' in parsed_url.netloc:
         return parsed_url.path[1:]
-
     return None
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    thumbnail_url = None
+    thumbnails = None
+    video_id = None
+
     if request.method == 'POST':
         video_url = request.form.get('video_url')
         video_id = extract_video_id(video_url)
         if video_id:
-            thumbnail_url = f"https://img.youtube.com/vi/{video_id}/maxresdefault.jpg"
-        else:
-            thumbnail_url = "invalid"
+            thumbnails = {
+                "Max Resolution": f"https://img.youtube.com/vi/{video_id}/maxresdefault.jpg",
+                "High Quality": f"https://img.youtube.com/vi/{video_id}/hqdefault.jpg",
+                "Medium Quality": f"https://img.youtube.com/vi/{video_id}/mqdefault.jpg",
+                "Default": f"https://img.youtube.com/vi/{video_id}/default.jpg"
+            }
 
-    return render_template('index.html', thumbnail_url=thumbnail_url)
+    return render_template('index.html', thumbnails=thumbnails, video_id=video_id)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
